e3a00f2a4afaf31620464ef3921c9533ead5bec4
grishunea80 <grishunea80@mail.ru>
2020-08-16 10:43:10 -0400

    sundayfunday

---
+++ src/test/tests/HomePageTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+public class HomePageTest extends BaseTest {
+    LogInPage logInPage;
+    HomePage homePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void localsetUp(){
+        setUp();
+        homePage = new HomePage(getDriver());
+
+    }
+    @Test
+    public void validatePageHeader(){
+        logInPage.logIn("standard");
+        Assert.assertEquals(homePage.headerText.getText(), "Products");
+    }
+
+}
